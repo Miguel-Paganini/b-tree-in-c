c77f0781d8016b6bb3159460ad0257d742f368da
Miguel-Paganini <128531851+Miguel-Paganini@users.noreply.github.com>
2026-03-16 08:52:21 -0300

    Update b-tree.c

---
+++ b-tree.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+#include "b-tree.h"
+
+Pagina *criarPagina() {
+    Pagina *pagina = (Pagina*) malloc(sizeof(Pagina));
+    if (!pagina) return NULL;
+    
+    pagina->qtd = 0;
+    pagina->folha = 1;
+    
+    for(int i=0; i < ORDEM-1; i++) {
+        pagina->chaves[i].valor = 0;
+        pagina->chaves[i].pos = 0;
+    }
+    
+    for(int i=0; i < ORDEM; i++) {
+        pagina->filho[i] = NULL;
+    }
+    
+    return pagina;
+}
+
+ArvoreB inserirArvB(ArvoreB raiz, Chave chave) {
+    
+}
+
