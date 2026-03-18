ce6e426403e9fb9bd92dc9886d0b7c0f096a5c93
Erlon90 <159197450+Erlon90@users.noreply.github.com>
2026-03-18 10:55:22 -0300

    implementacao busca arvB

--- b-tree.c
+++ b-tree.c
@@ -2,30 +2,58 @@
 #include <string.h>
 #include "b-tree.h"
 
-Pagina *criarPagina(int folha) {
+struct _chave {
+    int valor;
+    int pos;
+};
+
+struct _pagina {
+    int qtd;
+    Chave chaves[ORDEM - 1];
+    struct _pagina *filho[ORDEM];
+    int folha;
+};
+
+Pagina *criaPagina(int folha) {
     Pagina *pagina = (Pagina*) malloc(sizeof(Pagina));
     if (!pagina) return NULL;
-    
+
     pagina->qtd = 0;
     pagina->folha = folha;
-    
+
     for(int i=0; i < ORDEM-1; i++) {
         pagina->chaves[i].valor = 0;
         pagina->chaves[i].pos = 0;
     }
-    
+
     for(int i=0; i < ORDEM; i++) {
         pagina->filho[i] = NULL;
     }
-    
+
     return pagina;
 }
 
 ArvoreB criarArvB(){
-    return criarPagina(1);
+    return criaPagina(1);
+}
+
+int buscaArvB(Pagina *pagAtual, int chave, Pagina *pagChave, int *posChave){
+    if(pagAtual == NULL)
+        return 0;
+    else{
+        for(int i = 0; i < ORDEM - 1; i++){
+            if(chave ==  pagAtual->chaves[i].valor){
+                *pagChave = pagAtual;
+                *posChave = i;
+                return 1;
+            }
+            if (chave > pagAtual->chaves[i].valor) continue;
+            return buscaArvB(pagAtual->filho[i], chave, pagChave, posChave);
+        }
+        return buscaArvB(pagAtual->filho[ORDEM], chave, pagChave, posChave);
+    }
 }
 
 ArvoreB inserirArvB(ArvoreB raiz, Chave chave) {
-    
-}
 
+}
