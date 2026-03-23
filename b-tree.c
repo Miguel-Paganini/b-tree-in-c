51ca92253f33cb2fc81d74d8ed1374dfa3f39692
Miguel-Paganini <128531851+Miguel-Paganini@users.noreply.github.com>
2026-03-23 08:45:03 -0300

    Update b-tree.c

--- b-tree.c
+++ b-tree.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include "b-tree.h"
 
+/*
 struct _chave {
     int valor;
     int pos;
@@ -13,6 +14,7 @@ struct _pagina {
     struct _pagina *filho[ORDEM];
     int folha;
 };
+*/
 
 Pagina *criaPagina(int folha) {
     Pagina *pagina = (Pagina*) malloc(sizeof(Pagina));
@@ -21,12 +23,12 @@ Pagina *criaPagina(int folha) {
     pagina->qtd = 0;
     pagina->folha = folha;
 
-    for(int i=0; i < ORDEM-1; i++) {
+    for (int i=0; i < ORDEM-1; i++) {
         pagina->chaves[i].valor = 0;
         pagina->chaves[i].pos = 0;
     }
 
-    for(int i=0; i < ORDEM; i++) {
+    for (int i=0; i < ORDEM; i++) {
         pagina->filho[i] = NULL;
     }
 
@@ -38,22 +40,34 @@ ArvoreB criarArvB(){
 }
 
 int buscaArvB(Pagina *pagAtual, int chave, Pagina *pagChave, int *posChave){
-    if(pagAtual == NULL)
+    if (pagAtual == NULL)
         return 0;
-    else{
-        for(int i = 0; i < ORDEM - 1; i++){
-            if(chave ==  pagAtual->chaves[i].valor){
-                *pagChave = pagAtual;
+    else {
+        for (int i = 0; i < pagAtual->qtd; i++){
+            if (chave ==  pagAtual->chaves[i].valor){
+                *pagChave = *pagAtual;
                 *posChave = i;
                 return 1;
             }
             if (chave > pagAtual->chaves[i].valor) continue;
             return buscaArvB(pagAtual->filho[i], chave, pagChave, posChave);
         }
-        return buscaArvB(pagAtual->filho[ORDEM], chave, pagChave, posChave);
+        return buscaArvB(pagAtual->filho[pagAtual->qtd], chave, pagChave, posChave);
     }
 }
 
-ArvoreB inserirArvB(ArvoreB raiz, Chave chave) {
-
+ArvoreB inserirArvB(Pagina *pagAtual, int chave, Pagina *filhoProDir, int *chaveProm) {
+    if (*pagAtual == NULL) {
+        *chaveProm = chave;
+        *filhoProDir = NULL;
+        return PROMOTION;
+    }
+    else {
+        for (int i = 0; i < pagAtual->qtd; i++) {
+            if (chave == pagAtual->chaves[i].valor) {
+                printf("Chave %d ja existe na arvore.\n", chave);
+                return ERROR;
+            }
+        }
+    }
 }
