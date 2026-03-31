16d721d14f5b9d149f4d4cf765d72ba9b42bdf15
Miguel-Paganini <128531851+Miguel-Paganini@users.noreply.github.com>
2026-03-30 21:13:15 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,28 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "b-tree.h"
 
-int main() {
-    // Cria a raiz (não folha)
-    ArvoreB arv = criarArvB();
-    
-    arv = inserir(arv, 14);
-    arv = inserir(arv, 15);
-    arv = inserir(arv, 16);
-    arv = inserir(arv, 17);
-    
+int main(void)
+{
+    printf("Carregando registros de \"%s\"...\n", ARQUIVO_REGISTROS);
+    Pagina *raiz = carregarRegistros(ARQUIVO_REGISTROS);
+    printf("Árvore B carregada em memória.\n\n");
 
-    // Teste de busca
-    Pagina *pagEncontrada;
-    int pos;
-
-    for(int chaveBusca = 14; chaveBusca < 18; chaveBusca++){
-        if (buscaArvB(arv, chaveBusca, &pagEncontrada, &pos)) {
-            printf("Chave %d encontrada na posicao %d da pagina!\n", chaveBusca, pos);
-        } else {
-            printf("Chave %d nao encontrada.\n", chaveBusca);
-        }
-    }
+    executarMenu(&raiz);
 
     return 0;
 }
